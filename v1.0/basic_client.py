6ab29741de263e00c1ff28ccc6a684859e22e205
BugOrFeature <BugOrFeature@users.noreply.github.com>
2019-04-02 11:45:49 +0200

    Create basic_client.py

---
+++ v1.0/basic_client.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+url = "https://echo.aerlabs.com/v1/noise"
+
+with open('noise.json', 'r') as file:
+    data=file.read()
+
+token = input("please input your token")
+
+payload = data
+headers = {
+    'Content-Type': "application/json",
+    'Authorization': "Bearer " + token,
+    'cache-control': "no-cache",
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+print(response.text)
